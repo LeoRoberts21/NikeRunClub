dd91217d57772438de59591da3ec1d294f33f5f8
Leo Roberts <100042123+LeoRoberts21@users.noreply.github.com>
2023-03-09 22:06:18 +0000

    plans page

---
+++ plans.js
@@ -0,0 +1,4 @@
+homeBtn.addEventListener("mouseleave", plansHover);
+plansBtn.addEventListener("mouseleave", plansHover);
+clubBtn.addEventListener("mouseleave", plansHover);
+activityBtn.addEventListener("mouseleave", plansHover);
