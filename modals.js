8e70c12504fb20e3851d3bb111cf7fb2cc8d259e
Leo Roberts <100042123+LeoRoberts21@users.noreply.github.com>
2023-03-22 16:33:38 +0000

    second modal

---
+++ modals.js
@@ -0,0 +1,62 @@
+"use strict";
+
+//MODAL 1, ALL ACTIVITY, ACTIVITY PAGE
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+//MODAL 2, ALL ACHIEVEMENTS, ACTIVITY PAGE
+
+const modalTwo = document.querySelector(".modalTwo");
+const btnCloseModalTwo = document.querySelector(".close-modal-two");
+const btnsOpenModalTwo = document.querySelectorAll(".show-modal-two");
+
+const openModalTwo = function () {
+  modalTwo.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModalTwo = function () {
+  modalTwo.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModalTwo.length; i++)
+  btnsOpenModalTwo[i].addEventListener("click", openModalTwo);
+
+btnCloseModalTwo.addEventListener("click", closeModalTwo);
+overlay.addEventListener("click", closeModalTwo);
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModalTwo();
+  }
+});
